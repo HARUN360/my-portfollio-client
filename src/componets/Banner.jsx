be7bbb39592a53e6993b242f7bd27dea90bfe171
HARUN360 <mdharunorroshid301204@gmail.com>
2024-07-04 11:53:33 +0600

    home page added

--- src/componets/Banner.jsx
+++ src/componets/Banner.jsx
@@ -8,13 +8,14 @@ const Banner = () => {
                     <img
                         src="https://i.ibb.co/KhkVwcZ/IMG-20240703-173856-1.jpg"
                         className="max-w-sm  shadow-2xl rounded-full" />
-                    <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">
-                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                            quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    <div className="text-white">
+                        <h5 className="text-2xl font-bold">HELLO, MY NAME IS</h5>
+                        <h3 className="text-5xl  font-bold my-6">MD.HARUN OR ROSHID</h3>
+                        <p className="text-xl font-bold text-[#F5B714]">I AM Web Developer</p>
+                        <p className="py-6 lg:mr-20">
+                        From Dhaka, Bangladesh. I’m a Front End Web developer who is passionate about making error-free websites. I am good at React. I love to talk with you about your unique. 
                         </p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <button className="btn btn-outline bg-[#F8B90C] btn-[#262626] rounded-full text-2xl font-bold">Contact</button>
                     </div>
                 </div>
             </div>
